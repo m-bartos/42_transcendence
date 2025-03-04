7f6cf75a667907eb7d930aef5d2459f60ff71b8e
O.R <orezek@seznam.cz>
2025-03-04 15:23:05 +0100

    refactor:uncomment sqlite dir

--- services/auth_service/dist/index.js
+++ services/auth_service/dist/index.js
@@ -1,6 +1,5 @@
 import Fastify from 'fastify';
 import fp from 'fastify-plugin';
-import sqlite from 'fastify-sqlite-typed';
 import knexPlugin from "./plugins/knexPlugin.js";
 import routesPlugin from "./plugins/routesPlugin.js";
 import schemas from "./schemas.js";
@@ -12,13 +11,6 @@ await app.register(import('@fastify/jwt'), {
     secret: 'my-super-secret-key', // Hardcoded for testing
     sign: {
         expiresIn: '1h' // Initial expiration: 1 hour
-    }
-});
-// @ts-ignore
-app.register(sqlite, {
-    dbFilename: '/sqlite_db_data/auth_service.sqlite',
-    driverSettings: {
-        verbose: true,
     }
 });
 await app.register(knexPlugin);
@@ -33,4 +25,3 @@ app.ready()
 app.listen({ port: 3000, host: '0.0.0.0' }).then((address) => {
     console.log(`Server is running with : ${address}`);
 });
-export { app };
