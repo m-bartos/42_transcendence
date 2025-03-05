e255c1ee370f422822562efff07113c864887edd
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-03-05 11:12:51 +0100

    chore(auth_service): delete dist and node_modules folders

--- services/auth_service/dist/utils/postRequestUtils.js
+++
@@ -1,5 +0,0 @@
-function extractUsernameEmailPassword(request) {
-    const { username, email, password } = request.body;
-    return { username, email, password };
-}
-export { extractUsernameEmailPassword };
