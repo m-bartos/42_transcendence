49fb718494784515074c862ac0aad16c703a1eb5
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-03-07 09:34:53 +0100

    chore(auth-service): delete node_modules and dist folder

--- services/auth_service/dist/utils/dbQueries.js
+++
@@ -1,3 +0,0 @@
-export async function getUserId(instance, request) {
-    return instance.dbSqlite('sessions').select('user_id').where({ session_id: request.session_id, revoked: false }).andWhereRaw("UNIXEPOCH(expires_at) > UNIXEPOCH('now')").first();
-}
