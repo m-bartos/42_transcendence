3a795471d441ff697dfa121bb9bf3d36ed7504c7
O.R <orezek@seznam.cz>
2025-03-09 18:24:47 +0100

    feat:file_upload complete

--- services/file_upload_service/src/plugins/uploadPlugin.ts
+++ services/file_upload_service/src/plugins/uploadPlugin.ts
@@ -1,42 +1,63 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from 'fastify';
 import fp from 'fastify-plugin';
-import multipart from '@fastify/multipart';
+import multipart, {MultipartFile} from '@fastify/multipart';
 import { pipeline } from 'node:stream/promises';
 import { createWriteStream } from 'node:fs';
+import authenticate from "../utils/authenticate.js";
+
+
+async function updateUserProfile(filePath:string)
+{
+    const response: Response = await fetch('http://auth_service:3000/internal/avatar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ file: filePath }),
+        });
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+}
+
+
+async function uploadHandler(this: FastifyInstance, request: FastifyRequest, reply: FastifyReply) {
+    const file: MultipartFile | undefined = await request.file();
+    if (!file) {
+        reply.code(400);
+        return {status: 'error', message: 'no file uploaded'};
+    }
+    const filePath = `/static_data/${file.filename}`;
+    await pipeline(file.file, createWriteStream(filePath));
+    try
+    {
+        await updateUserProfile(filePath);
+        reply.code(200);
+        return {status: 'success', message: 'file uploaded successfully'};
+    }
+    catch (error: unknown)
+    {
+        reply.code(500);
+        if (error instanceof Error)
+        {
+            return {status: 'error', message: 'file upload failed'};
+        }
+        return {status: 'error', message: 'internal server error'};
+    }
+}
+
 
 
 export default fp(async function (fastify: FastifyInstance, options: FastifyPluginOptions) {
     fastify.register(multipart);
 
-    fastify.post('/upload', async (request: FastifyRequest, reply: FastifyReply) => {
-        const file = await request.file();
-        if (!file)
+    fastify.route(
         {
-            reply.code(400);
-            return {status: "error", message: "No file uploaded"};
+            method: 'post',
+            url: '/upload',
+            preHandler: authenticate,
+            handler: uploadHandler,
         }
+    )
 
-
-        console.log({
-            fieldname: file.fieldname,
-            filename: file.filename,
-            mimetype: file.mimetype,
-            encodingType: file.encoding,
-        });
-        // Stream the file to disk
-        const filePath = `/static_data/${file.filename}`;
-        await pipeline(file.file, createWriteStream(filePath));
-
-        // await fetch('http://auth_service:3000/avatar', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     // @ts-ignore
-        //     body: {file: filePath},
-        // });
-
-        reply.code(200);
-        return { message: 'File uploaded successfully' };
-    })
 });
