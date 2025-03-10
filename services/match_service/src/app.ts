92bdc86cc24783a40f7eabfbec422053d0125064
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-03-10 16:00:51 +0100

    refactor(deployment): change ports of game and match services

--- services/match_service/src/app.ts
+++ services/match_service/src/app.ts
@@ -49,7 +49,7 @@ export {options};
 //
 // const start = async () => {
 //     try {
-//         await fastify.listen({ port: 3002, host: '0.0.0.0' })
+//         await fastify.listen({ port: 3000, host: '0.0.0.0' })
 //     } catch (err) {
 //         fastify.log.error(err)
 //         process.exit(1)
