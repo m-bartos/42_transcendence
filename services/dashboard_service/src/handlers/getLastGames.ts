cac7ea39cac9b4de71c8ec27fe3d8135ae91f167
O.R <orezek@seznam.cz>
2025-04-09 12:51:48 +0200

    feat: add db connection to dashboard sevice

--- services/dashboard_service/src/handlers/getLastGames.ts
+++ services/dashboard_service/src/handlers/getLastGames.ts
@@ -1,6 +1,12 @@
 import type {FastifyInstance, FastifyRequest, FastifyReply} from "fastify";
+import {decryptUserId} from "../utils/secureUserId.js";
 
 export async function getLastGames(this: FastifyInstance, request: FastifyRequest, reply: FastifyReply) {
+    const encryptedUserId: string = request.jwt_payload.sub
+    const reqId = decryptUserId(encryptedUserId);
+
+    const result : number [] = await this.dbSqlite('game_results').select("*").where({winner_id: reqId});
+
     reply.code(200);
-    return {lastGames: "some games"};
+    return {lastGames: "some games", req: reqId, data: {result: result}};
 }
--- services/dashboard_service/src/index.ts
+++ services/dashboard_service/src/index.ts
@@ -1,12 +1,12 @@
 import fastify from 'fastify';
 import authPlugin from "./plugins/authPlugin.js";
 import routePlugin from "./plugins/routePlugin.js";
-import type { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import knexPlugin from "./plugins/knexPlugin.js";
 
 const app = fastify({logger: true});
 
 app.register(authPlugin);
 app.register(routePlugin);
+app.register(knexPlugin);
 
 await app.listen({port: 5005, host: "0.0.0.0"})
-
