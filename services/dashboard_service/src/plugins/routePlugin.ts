dd563adb23fe31ff889a9524142139ef9eab4f6c
O.R <orezek@seznam.cz>
2025-04-19 21:50:49 +0200

    feat: impolement pagination protype for dashboard service

--- services/dashboard_service/src/plugins/routePlugin.ts
+++ services/dashboard_service/src/plugins/routePlugin.ts
@@ -1,9 +1,10 @@
 import {FastifyInstance, FastifyPluginOptions} from "fastify";
 import fp from 'fastify-plugin';
-import { gamesWins } from "../routes/dashboardRoutes.js"
+import {gamesWins, getMultiGames} from "../routes/dashboardRoutes.js"
 
 async function routePlugin(fastify: FastifyInstance, opts: FastifyPluginOptions): Promise<void> {
     fastify.route(gamesWins);
+    fastify.route(getMultiGames)
 }
 
 export default fp(routePlugin)
--- services/dashboard_service/src/routes/dashboardRoutes.ts
+++ services/dashboard_service/src/routes/dashboardRoutes.ts
@@ -1,9 +1,17 @@
 import { getWins } from "../handlers/getLastGames.js";
 import authenticate  from '../utils/authenticate.js'
+import {getMultiplayerGames} from "../handlers/getMultiplayerGames.js";
 
 export const gamesWins = {
     url: "/games/wins",
     method: "GET",
     preHandler: authenticate,
     handler: getWins,
+}
+
+export const getMultiGames = {
+    url: "/games/last",
+    method: 'POST',
+    preHandler: authenticate,
+    handler: getMultiplayerGames
 }
