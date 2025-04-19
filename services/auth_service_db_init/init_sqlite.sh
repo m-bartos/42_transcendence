c97ef1bd697630d7606a9dc6aec68d877e0a859b
O.R <orezek@seznam.cz>
2025-04-19 23:03:57 +0200

    refactor: change db naming

--- services/auth_service_db_init/init_sqlite.sh
+++ services/auth_service_db_init/init_sqlite.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-DB_PATH="/sqlite_db_data/auth_service.sqlite"
+DB_PATH_AUTH="/auth_db_data/auth_service.sqlite"
 DB_PATH_DASHBOARD="/dashboard_db_data/dashboard_service.sqlite"
 DB_PATH_FRIENDS="/friends_db_data/friends_service.sqlite"
 
 # Ensure the SQLite database file exists
-echo "Checking SQLite file at $DB_PATH..."
-if [ ! -f "$DB_PATH" ]; then
-    echo "Creating new SQLite database at $DB_PATH"
-    touch "$DB_PATH"
+echo "Checking Auth DB file at $DB_PATH_AUTH..."
+if [ ! -f "$DB_PATH_AUTH" ]; then
+    echo "Creating new SQLite database at $DB_PATH_AUTH"
+    touch "$DB_PATH_AUTH"
 else
     echo "Auth service database already exists."
 fi
@@ -37,7 +37,7 @@ fi
 # Run Flyway migrations
 echo "Running Flyway migrations for auth_service"
 # patch to sqlite file                              #path to migrations files
-flyway migrate -url=jdbc:sqlite:$DB_PATH -locations=filesystem:/flyway/sql/sqlite
+flyway migrate -url=jdbc:sqlite:$DB_PATH_AUTH -locations=filesystem:/flyway/sql/auth
 
 echo "Running Flyway migration for dashboard_service"
 # patch to sqlite file                              #path to migrations files
