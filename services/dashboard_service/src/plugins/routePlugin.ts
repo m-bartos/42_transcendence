e93ce0618610bc56b348fcdf4d6f07f0c84c359b
O.R <orezek@seznam.cz>
2025-04-20 19:04:01 +0200

    feat: update dashboard service endpoints

--- services/dashboard_service/src/plugins/routePlugin.ts
+++ services/dashboard_service/src/plugins/routePlugin.ts
@@ -1,10 +1,10 @@
-import {FastifyInstance, FastifyPluginOptions} from "fastify";
+import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fp from 'fastify-plugin';
-import {gamesWins, getMultiGames} from "../routes/dashboardRoutes.js"
+import { getSplitGames, getMultiGames } from "../routes/dashboardRoutes.js"
 
 async function routePlugin(fastify: FastifyInstance, opts: FastifyPluginOptions): Promise<void> {
-    fastify.route(gamesWins);
     fastify.route(getMultiGames)
+    fastify.route(getSplitGames);
 }
 
 export default fp(routePlugin)
--- services/dashboard_service/src/routes/dashboardRoutes.ts
+++ services/dashboard_service/src/routes/dashboardRoutes.ts
@@ -1,17 +1,19 @@
-import { getWins } from "../handlers/getLastGames.js";
 import authenticate  from '../utils/authenticate.js'
-import {getMultiplayerGames} from "../handlers/getMultiplayerGames.js";
-
-export const gamesWins = {
-    url: "/games/wins",
-    method: "GET",
-    preHandler: authenticate,
-    handler: getWins,
-}
+import { getMultiplayerGames } from "../handlers/getMultiplayerGames.js";
+import { getSplitKeyboardGames } from "../handlers/getSplitKeyboardGames.js";
 
 export const getMultiGames = {
-    url: "/games/last",
+    url: "/multiplayer",
     method: 'POST',
     preHandler: authenticate,
     handler: getMultiplayerGames
+    // add schemas
+}
+
+export const getSplitGames = {
+    url: "/split",
+    method: 'POST',
+    preHandler: authenticate,
+    handler: getSplitKeyboardGames
+    // add schemas
 }
