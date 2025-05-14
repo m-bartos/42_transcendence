3595b8000e3b80a6d8194b39ae921de3df9070e9
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-05-14 23:38:15 +0200

    fix(game-service): delete ended games

--- services/game_service/src/plugins/game-plugin.ts
+++ services/game_service/src/plugins/game-plugin.ts
@@ -7,7 +7,7 @@ declare module 'fastify' {
         gameManager: gameManager.GameManager;
         broadcastLiveGames: ReturnType<typeof setInterval>;
         // broadcastPendingAndFinishedGames: ReturnType<typeof setInterval>;
-        // checkPendingGames: ReturnType<typeof setInterval>;
+        checkPendingGames: ReturnType<typeof setInterval>;
     }
 }
 
@@ -23,19 +23,20 @@ const gamePlugin: FastifyPluginAsync = async (fastify: FastifyInstance, options:
     //     fastify.gameManager.broadcastPendingAndFinishedGames(fastify);
     // }, 500));
 
-    // fastify.decorate('checkPendingGames', setInterval(() => {
-    //     fastify.gameManager.checkPendingGames(fastify);
-    // }, 1000));
+    fastify.decorate('checkPendingGames', setInterval(() => {
+        fastify.gameManager.checkPendingGames(fastify);
+    }, 1000));
 
     fastify.addHook('onClose', (instance, done) => {
         console.log('Closing game plugin');
         clearInterval(fastify.broadcastLiveGames);
         // clearInterval(fastify.broadcastPendingAndFinishedGames);
-        // clearInterval(fastify.checkPendingGames);
+        clearInterval(fastify.checkPendingGames);
         fastify.gameManager.closeAllWebSockets();
         done();
     });
 };
+
 
 export default fp(gamePlugin, {
     name: 'gamePlugin',
