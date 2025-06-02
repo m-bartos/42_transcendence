3b2ae1cf926e410d3f59570242c51cacb6e037a2
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-06-02 16:17:06 +0200

    fix(dashboard-service): splitkeyboard principalId search

--- services/dashboard_service/src/handlers/getSplitKeyboardGames.ts
+++ services/dashboard_service/src/handlers/getSplitKeyboardGames.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { decryptUserId } from "../utils/secureUserId.js";
 
 interface RequestBody {
-    userId: string;
+    userId: number;
     pagination: {
         limit: number;
         offset: number;
@@ -24,7 +24,7 @@ const responseData = {
 
 export async function getSplitKeyboardGames(this: FastifyInstance, request: FastifyRequest, reply: FastifyReply) {
     const encryptedUserId: string = request.jwt_payload.sub
-    const authUserId: string = decryptUserId(encryptedUserId);
+    // const authUserId: string = decryptUserId(encryptedUserId);
     const {userId, pagination} = request.body as RequestBody;
     console.log('User Id:',userId);
     console.log('Pagination:',pagination);
@@ -40,10 +40,28 @@ export async function getSplitKeyboardGames(this: FastifyInstance, request: Fast
     try
     {
         // get number of records
-        const result = await this.dbSqlite('split_keyboard_results').where(function () {this.where('player_one_id', userId).orWhere('player_two_id', userId);}).count('* as count');
+        const result = await this.dbSqlite('split_keyboard_results').where(function () {this.where('principal_id', userId);}).count('* as count');
         const count = result[0].count;
         const total = Number(count);
-        const games = await this.dbSqlite('split_keyboard_results').select('*').where({player_one_id: userId}).orWhere({player_two_id: userId}).orderBy('created_at', 'desc').limit(limit).offset(offset);
+        const games = await this.dbSqlite('split_keyboard_results').select(
+        'id',
+        // 'game_id',
+        'game_mode',
+        'end_reason',
+        'principal_id',
+        'player_one_username',
+        'player_one_score',
+        'player_one_paddle_bounce',
+        'player_two_username',
+        'player_two_score',
+        'player_two_paddle_bounce',
+        'created_at',
+        'started_at',
+        'ended_at',
+        'duration_seconds',
+        'winner_username',
+        'loser_username'
+        ).where({'principal_id': userId}).orderBy('created_at', 'desc').limit(limit).offset(offset);
 
         // build pagination data - need to review this build function
         const hasPrev = offset > 0;
