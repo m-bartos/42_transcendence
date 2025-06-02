b8d3a65c1ccf9ba76a421ee07e2de30f9b7567e1
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-06-02 10:03:18 +0200

    fix(dashboard-service): register CORS plugin

--- services/dashboard_service/src/index.ts
+++ services/dashboard_service/src/index.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify';
 import authPlugin from "./plugins/authPlugin.js";
 import routePlugin from "./plugins/routePlugin.js";
 import knexPlugin from "./plugins/knexPlugin.js";
+import corsPlugin from "./plugins/corsPlugin.js";
 
 const app = fastify({logger: true});
 
 app.register(authPlugin);
 app.register(routePlugin);
 app.register(knexPlugin);
+app.register(corsPlugin);
 
 await app.listen({port: 3000, host: "0.0.0.0"})
