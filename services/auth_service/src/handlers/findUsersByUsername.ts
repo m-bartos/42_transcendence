2198aa4f0864057547932a093ce03bda1f857ef7
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-06-02 14:49:18 +0200

    feat(auth-service): fulltext search

---
+++ services/auth_service/src/handlers/findUsersByUsername.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+
+interface FindUsersByUsernameRequestQuery {
+    username: string;
+}
+
+interface AuthServiceResponseBody {
+    status:  "success" | "error";
+    message: string;
+    data?:   unknown;
+}
+
+interface AuthServiceResponseData {
+    user_id:    string;
+    username:   string;
+    avatar_url: string;
+}
+
+async function findUsersByUsername(this: FastifyInstance, request: FastifyRequest, reply: FastifyReply): Promise<AuthServiceResponseBody> {
+    const { username } = request.query as FindUsersByUsernameRequestQuery;
+    try {
+        // sqlite has limit max 999 results returned on this 'in' query
+        const users = await this.dbSqlite<AuthServiceResponseData>("users").select("id", "username", "avatar").whereLike("username", '%' + username + '%').where("active", true);
+        reply.code(200);
+        return { status: "success", message: "user info", data: users };
+    }
+    catch (err) {
+        reply.code(500);
+        return { status: "error", message: "internal server error" };
+    }
+}
+
+export default findUsersByUsername;
