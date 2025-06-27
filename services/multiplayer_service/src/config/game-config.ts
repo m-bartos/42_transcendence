44098d07a88f2e04fb008d570abd2c154ed1d1d6
m-bartos <28412526+m-bartos@users.noreply.github.com>
2025-06-27 15:50:04 +0200

    feat: update game configs

--- services/multiplayer_service/src/config/game-config.ts
+++ services/multiplayer_service/src/config/game-config.ts
@@ -4,16 +4,16 @@ export const CANVAS_WIDTH = 200;
 export const CANVAS_HEIGHT = 100;
 
 export const PADDLE_INIT_POSITION = 50;
-export const PADDLE_MOVE_STEP = 1.15;
-export const PADDLE_WIDTH = 0.5; // X % of field width
-export const PADDLE_HEIGHT = 22.5;  // X % of field height
+export const PADDLE_MOVE_STEP = 1.1;
+export const PADDLE_WIDTH = 1; // X % of field width
+export const PADDLE_HEIGHT = 20;  // X % of field height
 
 
 export const BALL_SEMIDIAMETER = 1; // this we agreed on
 export const BALL_DIAMETER = BALL_SEMIDIAMETER * 2;
 export const BALL_INIT_SPEED = 1.1; // 0.5 - 0.9 for start
 export const BALL_SPEED_INCREMENT = 1.1; // 10% speed increase per hit
-export const BALL_MAX_SPEED = 2.3; // till 1.5-2 playable
+export const BALL_MAX_SPEED = 2; // till 1.5-2 playable
 export const BALL_START_X = CANVAS_WIDTH / 2;
 export const BALL_START_Y = CANVAS_HEIGHT / 2;
 
